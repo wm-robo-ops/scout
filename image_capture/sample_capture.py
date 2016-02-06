8809c80f85e532b9f0b01d147bb062647d3af95a
Afnan Enayet <afnan.enayet@gmail.com>
2016-02-06 15:12:44 -0500

    Fixed file naming

--- image_capture/sample_capture.py
+++ image_capture/sample_capture.py
@@ -3,6 +3,8 @@ import picamera
 from time import sleep
 
 camera = picamera.PiCamera()
+camera.start_preview()
+sleep(2)
 is_recording = False
 
 def record(duration, enable_stabilization, filename):
@@ -14,6 +16,8 @@ def record(duration, enable_stabilization, filename):
     camera.video_stabilization = enable_stabilization
 
     camera.start_preview()
+    # camera warm-up pi
+    sleep(2)
     camera.start_recording(filename)
     print("Started recording")
 
@@ -31,10 +35,10 @@ def capture_image(is_repeating, interval_length, iterations, filename, file_exte
     If iterations = -1, then image capture will be continuous until
     told to stop'''
     counter = 1
-    camera.start_preview()
+    
     if iterations == -1:
         while is_repeating:
-            camera.capture(filename, str(counter), file_extension)
+            camera.capture(filename + str(counter) + file_extension)
             print("Captured image (continuous)")
             # Making sure we don't overwrite files
             counter = counter + 1
@@ -42,7 +46,7 @@ def capture_image(is_repeating, interval_length, iterations, filename, file_exte
     else:
         for i in range(iterations):
             print("Captured image (iterations)")
-            camera.capture(filename, str(i + 1), file_extension)
+            camera.capture(filename + str(i + 1) + file_extension)
             sleep(duration)
 
 
@@ -62,7 +66,7 @@ def stop_img_capture():
 # Recording a video for 30 seconds
 record(5, False, "test_video.h264")
 
-# Capturing image every 5 seconds 10 times as a .jpg
+# Capturing image every 1 second 2 times as a .jpg
 capture_image(True, 1, 2, "test_image", ".jpg")
 
 camera.close()
