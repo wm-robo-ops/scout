89c4cb47af22c1f94032be0d1a41938cfab96db7
Afnan Enayet <afnan.enayet@gmail.com>
2016-02-16 18:07:32 -0500

    Added console statements so user will have a better idea of what's going on

--- image_capture/stream_video_headless.py
+++ image_capture/stream_video_headless.py
@@ -3,17 +3,20 @@ import time
 import picamera
 
 with picamera.PiCamera() as camera:
+    print("Camera initialized")
     camera.resolution = (640, 480)
     camera.framerate = 24
 
     server_socket = socket.socket()
     server_socket.bind(('0.0.0.0', 12345))
     server_socket.listen(0)
+    print("Listening for connection...")
 
     # Accept a single connection and make a file-like object out of it
     connection = server_socket.accept()[0].makefile('wb')
     try:
         camera.start_recording(connection, format='h264')
+        print("Recording has started")
         camera.wait_recording(60)
         camera.stop_recording()
     finally:
