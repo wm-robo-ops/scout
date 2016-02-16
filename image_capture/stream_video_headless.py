f376ea1dd236aff62fd7f911e1251603c69f9106
Afnan Enayet <afnan.enayet@gmail.com>
2016-02-16 18:11:34 -0500

    Started adding configurable stream options

--- image_capture/stream_video_headless.py
+++ image_capture/stream_video_headless.py
@@ -1,24 +1,33 @@
 import socket
 import time
 import picamera
+       
+def start_streaming(port):
+    with picamera.PiCamera() as camera:
+        print("Camera initialized")
+        camera.resolution = (640, 480)
+        camera.framerate = 24
 
-with picamera.PiCamera() as camera:
-    print("Camera initialized")
-    camera.resolution = (640, 480)
-    camera.framerate = 24
+        server_socket = socket.socket()
+        server_socket.bind(('0.0.0.0', 12345))
+        server_socket.listen(0)
+        print("Listening for connection...")
 
-    server_socket = socket.socket()
-    server_socket.bind(('0.0.0.0', 12345))
-    server_socket.listen(0)
-    print("Listening for connection...")
+        # Accept a single connection and make a file-like object out of it
+        connection = server_socket.accept()[0].makefile('wb')
+        try:
+            camera.start_recording(connection, format='h264')
+            print("Recording has started")
+            camera.wait_recording(60)
+            camera.stop_recording()
+        finally:
+            connection.close()
+            server_socket.close()
+            
+'''def set_resolution(x_res, y_res):
+    pi_camera = picamera.PiCamera()
+    pi_camera.resolution(x_res, y_res)
 
-    # Accept a single connection and make a file-like object out of it
-    connection = server_socket.accept()[0].makefile('wb')
-    try:
-        camera.start_recording(connection, format='h264')
-        print("Recording has started")
-        camera.wait_recording(60)
-        camera.stop_recording()
-    finally:
-        connection.close()
-        server_socket.close()
+def set_framerate(fps):
+    pi_camera.framerate = fps'''
+#TODO make a class out of this
